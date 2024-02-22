e754c0af04627766473ec7eef52a5882fb0e5727
Leon <leon@akaai.kr>
2024-02-22 18:36:11 +0900

    feat: BaseViewModel

--- app/src/main/java/com/example/usan/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/usan/feature/home/HomeViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.usan.feature.home
 
+import androidx.lifecycle.viewModelScope
 import com.example.usan.base.viewmodel.BaseViewModel
+import com.example.usan.base.viewmodel.MutableEventFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel : BaseViewModel() {
+
+
 }
